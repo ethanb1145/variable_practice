9aafaad59e41b2b0fc17448f31dd1193f308aa0e
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-06-16 14:50:57 -0600

    Reassign values and use command puts

--- variables.rb
+++ variables.rb
@@ -10,3 +10,9 @@ length = 52.89
 air = false
 water = false
 ground = true
+task = "using data types"
+name = "Ethan B"
+day = "very good"
+puts "The school for " + name + " is Turing"
+puts 2004 + 18
+puts "Hello, the current task is " + task + "!" 
